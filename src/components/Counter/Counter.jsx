ea71b36538acebcd105b8913652d3af5652b22da
Gabriel <gabriel.bachelor@gmail.com>
2025-09-04 10:26:18 +0000

    practice

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,100 @@
+import { useState, memo, useCallback, useMemo, useEffect } from "react";
+//useCallback is used to avoid the recreation of a function
+import IconButton from "../UI/IconButton.jsx";
+import MinusIcon from "../UI/Icons/MinusIcon.jsx";
+import PlusIcon from "../UI/Icons/PlusIcon.jsx";
+import CounterOutput from "./CounterOutput.jsx";
+import { log } from "../../log.js";
+import CounterHistory from "./CounterHistory.jsx";
+
+function isPrime(number) {
+  log("Calculating if is prime number", 2, "other");
+  if (number <= 1) {
+    return false;
+  }
+
+  const limit = Math.sqrt(number);
+
+  for (let i = 2; i <= limit; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const Counter = memo(function Counter({ initialCount }) {
+  log("<Counter /> rendered", 1);
+  //useMemo is used to prevent normal function to re render
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
+
+  // const [counter, setCounter] = useState(initialCount);
+
+  // // these functions are recreated at each redner, therefore memo will not work on the icons
+  // //useCallback wont allow these functions to be recreated
+  // const handleDecrement = useCallback(function handleDecrement() {
+  //   setCounter((prevCounter) => prevCounter - 1);
+  // }, []);
+
+  // const handleIncrement = useCallback(function handleIncrement() {
+  //   setCounter((prevCounter) => prevCounter + 1);
+  // }, []);
+
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([
+    { value: initialCount, id: Math.random() * 1000 },
+  ]);
+  // added an object to the state with value and id property to tie a key to it
+  //otherwise the state will jump around
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
+
+//   useEffect(() => {
+//     setCounterChanges(   [ { value: initialCount, id: Math.random() * 1000 }],
+// )
+//   }, [initialCount]);
+
+
+  const handleDecrement = useCallback(function handleDecrement() {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: -1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
+
+  const handleIncrement = useCallback(function handleIncrement() {
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: +1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
+
+  return (
+    <section className="counter">
+      <p className="counter-info">
+        The initial counter value was <strong>{initialCount}</strong>. It{" "}
+        <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
+      </p>
+      <p>
+        <IconButton icon={MinusIcon} onClick={handleDecrement}>
+          Decrement
+        </IconButton>
+        <CounterOutput value={currentCounter} />
+        <IconButton icon={PlusIcon} onClick={handleIncrement}>
+          Increment
+        </IconButton>
+      </p>
+      <CounterHistory history={counterChanges} />
+    </section>
+  );
+});
+
+export default Counter;
